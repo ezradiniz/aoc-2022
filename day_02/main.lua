84370823ae6e286947420f2a6e49e5e076f7c4bb
Mateus Esdras <linux.esdras@gmail.com>
2022-12-02 21:04:42 -0300

    add day 02

---
+++ day_02/main.lua
@@ -0,0 +1,81 @@
+local function read_lines(filename, callback)
+	local file = io.open(filename, "r")
+	if not file then
+		error("error: could not read the file: " .. filename)
+	end
+	for line in file:lines() do
+		callback(line)
+	end
+	file:close()
+end
+
+local function calc_score(opp, you)
+	local score_table = {}
+
+	score_table["A"] = {}
+	score_table["A"]["X"] = 1 + 3
+	score_table["A"]["Z"] = 3 + 0
+	score_table["A"]["Y"] = 2 + 6
+
+	score_table["B"] = {}
+	score_table["B"]["X"] = 1 + 0
+	score_table["B"]["Z"] = 3 + 6
+	score_table["B"]["Y"] = 2 + 3
+
+	score_table["C"] = {}
+	score_table["C"]["X"] = 1 + 6
+	score_table["C"]["Z"] = 3 + 3
+	score_table["C"]["Y"] = 2 + 0
+
+	return score_table[opp][you]
+end
+
+local function solve1()
+	local input = "./input.txt"
+
+	local score = 0
+	read_lines(input, function(line)
+		local opp = line:sub(1, 1)
+		local you = line:sub(3, 3)
+		score = score + calc_score(opp, you)
+	end)
+
+	print("part 1:", score)
+end
+
+local function solve2()
+	local input = "./input.txt"
+
+	local follow_strategy = function(opp, you)
+		local strategy = {}
+
+		strategy["A"] = {}
+		strategy["A"]["X"] = "Z"
+		strategy["A"]["Z"] = "Y"
+		strategy["A"]["Y"] = "X"
+
+		strategy["B"] = {}
+		strategy["B"]["X"] = "X"
+		strategy["B"]["Z"] = "Z"
+		strategy["B"]["Y"] = "Y"
+
+		strategy["C"] = {}
+		strategy["C"]["X"] = "Y"
+		strategy["C"]["Z"] = "X"
+		strategy["C"]["Y"] = "Z"
+
+		return strategy[opp][you]
+	end
+
+	local score = 0
+	read_lines(input, function(line)
+		local opp = line:sub(1, 1)
+		local you = line:sub(3, 3)
+		score = score + calc_score(opp, follow_strategy(opp, you))
+	end)
+
+	print("part 2:", score)
+end
+
+solve1()
+solve2()
